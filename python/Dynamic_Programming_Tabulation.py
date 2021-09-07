e8eddb0da6766fdb3e583d7aef905034f5cffdc5
Mati02K <mati02official@gmail.com>
2021-09-07 13:27:09 +0530

    Grid Traveller Problem Done

--- python/Dynamic_Programming_Tabulation.py
+++ python/Dynamic_Programming_Tabulation.py
@@ -18,6 +18,30 @@ def fibonacci(n):
 
 	return table[n]
 
+# Grid Traveller Problem
+# Given a 2D Array we have to find the number of possible ways to reach it
+# Since moving is possible in left and bottom directions we are just adding the numbers.
+# [1,1] is pre-destinated as 1 as it is possible to travel from one to one and we are going to use it to traverse
+
+def gridTraveller(m, n):
+# 	First Create a Table and empty all the elements
+#   m--> rows and n--> columns; incrementing by one their size so all situations is checked
+	table = [[0] * (n+1) for i in range(m+1)]
+# 	Setting the 1,1 position as 1 for traversal
+	table[1][1] = 1
+	for i in range(0,m+1):
+		for j in range(0, n+1):
+			current = table[i][j]
+			if j+1 <= n:  # This Condition is placed so that it wont go out of bounds in left direction
+				table[i][j+1] += current #Adding the left adjacent element
+			if i+1 <= m: # This Condition is placed so that it wont go out of bounds in down direction
+				table[i+1][j] += current #Adding the below adjacent element
+
+	return table[m][n]
+
 # Driver Code
 if __name__ == '__main__':
+	# Printing Fibonacci
 	print(fibonacci(100))
+	# Printing GridTraveller
+	print(gridTraveller(18,18))
