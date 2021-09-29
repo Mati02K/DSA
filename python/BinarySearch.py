8a4037154bcd963cf21ff68cf5ec62a9d32dde08
Mati02K <mati02official@gmail.com>
2021-09-29 15:45:02 +0530

    Binary Search Peak Element has been added

--- python/BinarySearch.py
+++ python/BinarySearch.py
@@ -72,7 +72,36 @@ def find_all_occurances(numbers, number_to_find):
 
     return sorted(indices)
 
+# Find Peak Element in an Array
+def findPeak(arr):
+    if len(arr) == 1:
+        return arr[0]
 
+    left = arr[0]
+    right = arr[-1]
+
+    if right > left: # Array is sorted in ascending order and not rotated
+        return right
+
+    if left > arr[1]: # Array is sorted in descending order and not rotated
+        return left
+
+    # Array is rotated sorted then proceed to find the peak element
+    start = 0
+    end = len(arr) - 1
+
+    while start < end:
+        mid = (start + end) // 2
+        if mid + 1 <= end and arr[mid] > arr[mid + 1] and arr[mid] > arr[mid - 1]:
+            return arr[mid]
+        elif arr[mid] < arr[mid-1]:
+            return arr[mid-1]
+        elif arr[mid] > right:  # If my mid element is greater than last element then the element is in the first half of the array,(i.e befor peak)
+            start= mid+1
+        else: # Else my element is after peak portion
+            end = mid-1
+
+    return arr[start]
     
 
 if __name__ == '__main__':
@@ -89,3 +118,14 @@ if __name__ == '__main__':
     numbers_to_find = 15  
     print(find_all_occurances(numbers,numbers_to_find))
     print("{number_to_find} is found in",find_all_occurances(numbers,numbers_to_find))
+
+    elements = [
+        [1, 2],
+        [11, 12, 13, 14, 15, 16, 10, 9, 8],
+        [1],
+        [2, 3, 4],
+        [3, 1, 2],
+        [25, 24, 23, 22, 21, 20]
+    ]
+    for element in elements:
+        print(findPeak(element))
